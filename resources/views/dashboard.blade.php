d6053471683ddeae922339fb483080abe3f6cbda
RoyAyo <roylayindeay0@gmail.com>
2020-02-15 12:08:01 +0100

    changed the default redirects

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,7 @@
+@extends('layouts.template')
+@section('content')
 <form id="logout-form" action="{{ route('logout') }}" method="POST">
     @csrf
 	<button type="submit">Logout</a>
 </form>
+@eendsection
